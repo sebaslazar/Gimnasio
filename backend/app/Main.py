7ad748f34e4d924c1b65f8423f5b182953affcbd
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2024-08-31 21:52:50 -0500

    DB Update

--- backend/app/Main.py
+++ backend/app/Main.py
@@ -1,17 +1,29 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter
-
-app = FastAPI()
-
-router = APIRouter()
+from app.config import db
 
 
-@router.get("/")
-async def home():
-    return "Bienvenido"
+def init_app():
+    db.init()
+
+    app = FastAPI(
+        title="Gymcontrol",
+        description="Control of a Gym",
+        version="1"
+    )
+
+    @app.on_event("startup")
+    async def startup():
+        await db.create_all()
+
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.close()
+
+    return app
 
 
-app.include_router(router)
+app = init_app()
 
 
 def start():
