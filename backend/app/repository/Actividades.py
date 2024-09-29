305f044dfc824812b2ebd6640aa2f0b64abc31f2
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2024-09-29 10:26:05 -0500

    Repo Actividades upgrade

--- backend/app/repository/Actividades.py
+++ backend/app/repository/Actividades.py
@@ -1,6 +1,25 @@
+from typing import List
+from sqlalchemy.future import select
+
 from app.model.Actividades import Actividad
 from app.repository.base_repo import BaseRepo
+from app.config import db, commit_rollback
 
 
 class ActividadRepositorio(BaseRepo):
     model = Actividad
+
+    @staticmethod
+    async def encontrar_por_nombre(nombre: str):
+        query = select(Actividad).where(Actividad.nombre == nombre)
+        return (await db.execute(query)).scalar_one_or_none()
+
+    @staticmethod
+    async def encontrar_por_lista_nombre(nombre: List[str]):
+        query = select(Actividad).where(Actividad.nombre.in_(nombre))
+        return (await db.execute(query)).scalars().all()
+
+    @staticmethod
+    async def crear_lista(nombre: List[Actividad]):
+        db.add_all(nombre)
+        await commit_rollback()
