998edc41b2c514d5e96ee1d25cda40296bb94fa0
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2024-11-07 20:11:59 -0500

    Alembic upgrated

--- backend/app/model/__init__.py
+++ backend/app/model/__init__.py
@@ -1,7 +1,6 @@
 from .Administradores import Administrador
 from .Proveedores import Proveedor
 from .compra_membresia import CompraMembresia
-from .actividad_entrenador import ActividadEntrenador
 from .Membresias import Membresia
 from .Actividades import Actividad
 from .Citas import Cita
--- backend/app/repository/actividad_entrenador.py
+++
@@ -1,6 +0,0 @@
-from app.model.actividad_entrenador import ActividadEntrenador
-from app.repository.base_repo import BaseRepo
-
-
-class ActividadEntrenadorRepository(BaseRepo):
-    model = ActividadEntrenador
