8ec8187fdbe77eb600dd071af27ebc3f4d5961f3
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2024-11-16 20:03:24 -0500

    Proveedor service added

--- backend/app/schema.py
+++ backend/app/schema.py
@@ -30,32 +30,20 @@ class SchemaRegistrar(BaseModel):
 
     # Validación de número de cédula
     @field_validator("ID")
-    def validar_id(cls, id_para_validar):
+    def check_id(cls, id_para_validar):
         logger.debug(f"Cédula en validación: {id_para_validar}")
-
-        # Validación por expresión regular
-        regex_cedula_10_digitos = r"\b[0-9]\.{0,1}[0-9]{3}\.{0,1}[0-9]{3}\.{0,1}[0-9]{3}\b"
-        regex_cedula_8_digitos = r"\b[0-9]{2}\.{0,1}[0-9]{3}\.{0,1}[0-9]{3}\b"
-        if (id_para_validar and not re.search(regex_cedula_10_digitos, id_para_validar, re.I)
-                and not re.search(regex_cedula_8_digitos, id_para_validar, re.I)):
-            raise HTTPException(status_code=400, detail="Tipo de cédula inválida")
-        return id_para_validar
+        if validar_identificacion(id_para_validar, "Tipo de cédula inválida"):
+            return id_para_validar
 
     # Validación de número de teléfono
     @field_validator("telefono")
-    def validar_telefono(cls, numero_para_validar):
+    def check_telefono(cls, numero_para_validar):
         logger.debug(f"Teléfono en validación: {numero_para_validar}")
+        if validar_telefono(numero_para_validar, "Número de teléfono inválido"):
+            return numero_para_validar
 
-        # Validación por expresión regular
-        regex = (r"^(\(\+?\d{2,3}\)[\*|\s|\-|\.]?(([\d][\*|\s|\-|\.]?){6})(([\d][\s|\-|\.]?){2})?|(\+?[\d]["
-                 r"\s|\-|\.]?){8}(([\d][\s|\-|\.]?){2}(([\d][\s|\-|\.]?){2})?)?)$")
-        if numero_para_validar and not re.search(regex, numero_para_validar, re.I):
-            raise HTTPException(status_code=400, detail="Número de teléfono inválido")
-        return numero_para_validar
-
-    # Validación de sexo
     @field_validator("sexo")
-    def validar_sexo(cls, sexo_para_validar):
+    def check_sexo(cls, sexo_para_validar):
         if hasattr(Sexo, sexo_para_validar) is False:
             raise HTTPException(status_code=400, detail="Género inválido")
         return sexo_para_validar
@@ -71,6 +59,34 @@ class SchemaEliminar(BaseModel):
     ID: str
 
 
+class SchemaProveedor(BaseModel):
+    ID_proveedor: str
+    ID_admin_creador: str
+    nombre: str
+    telefono: str
+    direccion: str
+    correo: str
+    producto: str
+
+    @field_validator("ID_proveedor")
+    def check_id_proveedor(cls, id_para_validar):
+        logger.debug(f"Cédula en validación: {id_para_validar}")
+        if validar_identificacion(id_para_validar, "Cédula de proveedor inválida"):
+            return id_para_validar
+
+    @field_validator("ID_admin_creador")
+    def check_id_admin(cls, id_para_validar):
+        logger.debug(f"Cédula en validación: {id_para_validar}")
+        if validar_identificacion(id_para_validar, "Cédula de administrador inválida"):
+            return id_para_validar
+
+    @field_validator("telefono")
+    def check_telefono(cls, numero_para_validar, mensaje_de_error):
+        logger.debug(f"Teléfono en validación: {numero_para_validar}")
+        if validar_telefono(numero_para_validar, "Teléfono de proveedor inválido"):
+            return numero_para_validar
+
+
 class SchemaDetallado(BaseModel):
     estado: str
     mensaje: str
@@ -80,3 +96,20 @@ class SchemaDetallado(BaseModel):
 class SchemaRespuesta(BaseModel):
     detalles: str
     resultado: Optional[T] = None
+
+
+def validar_identificacion(id_para_validar, mensaje_de_error):
+    regex_cedula_10_digitos = r"\b[0-9]\.{0,1}[0-9]{3}\.{0,1}[0-9]{3}\.{0,1}[0-9]{3}\b"
+    regex_cedula_8_digitos = r"\b[0-9]{2}\.{0,1}[0-9]{3}\.{0,1}[0-9]{3}\b"
+    if (id_para_validar and not re.search(regex_cedula_10_digitos, id_para_validar, re.I)
+            and not re.search(regex_cedula_8_digitos, id_para_validar, re.I)):
+        raise HTTPException(status_code=400, detail=mensaje_de_error)
+    return True
+
+
+def validar_telefono(numero_para_validar, mensaje_de_error):
+    regex = (r"^(\(\+?\d{2,3}\)[\*|\s|\-|\.]?(([\d][\*|\s|\-|\.]?){6})(([\d][\s|\-|\.]?){2})?|(\+?[\d]["
+             r"\s|\-|\.]?){8}(([\d][\s|\-|\.]?){2}(([\d][\s|\-|\.]?){2})?)?)$")
+    if numero_para_validar and not re.search(regex, numero_para_validar, re.I):
+        raise HTTPException(status_code=400, detail=mensaje_de_error)
+    return True
--- backend/app/service/Administradores.py
+++ backend/app/service/Administradores.py
@@ -2,6 +2,12 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import defer
 from app.model import Administrador
 from app.config import db
+from fastapi import HTTPException
+
+from app.schema import SchemaProveedor
+from app.model.Proveedores import Proveedor
+from app.repository.Administradores import AdministradorRepository
+from app.repository.Proveedores import ProveedorRepository
 
 
 class ServicioAdministrador:
@@ -10,3 +16,19 @@ class ServicioAdministrador:
     async def buscar_perfil_de_administrador(id_admin: str):
         query = select(Administrador).where(Administrador.ID_admin == id_admin).options(defer(Administrador.password))
         return (await db.execute(query)).scalar_one_or_none()
+
+    @staticmethod
+    async def agregar_proveedor(registro: SchemaProveedor):
+        _admin = await AdministradorRepository.buscar_por_id(model_id=registro.ID_admin_creador, name_id="ID_admin")
+        if _admin:
+            _id_proveedor = await ProveedorRepository.buscar_por_id(model_id=registro.ID_proveedor,
+                                                                    name_id="ID_proveedor")
+            if not _id_proveedor:
+                _proveedor = Proveedor(ID_proveedor=registro.ID_proveedor, ID_admin_creador=registro.ID_admin_creador,
+                                       nombre=registro.nombre, telefono=registro.telefono, direccion=registro.direccion,
+                                       correo=registro.correo, producto=registro.producto)
+                await ProveedorRepository.crear(**_proveedor.model_dump())
+            else:
+                raise HTTPException(status_code=404, detail="El proveedor ya existe")
+        else:
+            raise HTTPException(status_code=404, detail="El administrador no existe")
