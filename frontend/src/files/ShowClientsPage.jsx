f447a710e63b773eb5b666af35024c9734c41421
LUMAGOGO <l.rodriguez3@utp.edu.co>
2024-11-26 20:52:42 -0500

    Implement Table component

--- frontend/src/files/ShowClientsPage.jsx
+++ frontend/src/files/ShowClientsPage.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet";
 import { MainLayout } from "../layouts/PageLayout";
+import { Table, Tbody } from '../components/Table';
 
 export function ClientsPage() {
   return (
@@ -8,7 +9,32 @@ export function ClientsPage() {
         <title>Gymcontrol - Clientes</title>
       </Helmet>
       <MainLayout>
-        <h2>Clientes</h2>
+        <div style={{margin: 'auto', width: 'fit-content'}}>
+          <Table>
+            <thead>
+              <tr>
+                <Table.Th>Nombre</Table.Th>
+                <Table.Th>Identificacion</Table.Th>
+                <Table.Th>Telefono</Table.Th>
+                <Table.Th>Sexo</Table.Th>
+              </tr>
+            </thead>
+            <Tbody>
+              <tr>
+                <Table.Td>Carlos Andres Mauricio Santana</Table.Td>
+                <Table.Td>123456789</Table.Td>
+                <Table.Td>1234567</Table.Td>
+                <Table.Td>Masculino</Table.Td>
+              </tr>
+              <tr>
+                <Table.Td>Andrea</Table.Td>
+                <Table.Td>987654321</Table.Td>
+                <Table.Td>7654321</Table.Td>
+                <Table.Td>Femenino</Table.Td>
+              </tr>
+            </Tbody>
+          </Table>
+        </div>
       </MainLayout>
     </>
   );
