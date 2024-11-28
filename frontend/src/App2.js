df271b9a551112df952e0ea7988baed3f891d005
LUMAGOGO <l.rodriguez3@utp.edu.co>
2024-11-27 21:17:03 -0500

    Implements the new page in the correct route and implements protected section

--- frontend/src/App2.js
+++ frontend/src/App2.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React from "react";
 import { Helmet } from "react-helmet";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Home.js";
 import Login from './files/UI-02.js';
 import RegistroCliente from './files/UI-03.js';
 import { useUser } from "./contexts/UserContext.jsx";
 import { MainLayout } from './layouts/MainLayout.jsx';
 import { ClientsPage } from './files/ShowClientsPage.jsx';
+import { ProtectedRoutes } from './components/ProtectedRoute.jsx';
 
 document.documentElement.lang = "es"; //Establece el idioma de la página en español
 
@@ -21,11 +22,15 @@ function App() {
         <title>Gymcontrol</title>
       </Helmet>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/" element={<ClientsPage />} />
+        <Route path="/" element={<Home />} />
         <Route element={<MainLayout />}>
           <Route path="/login" element={!token ? <Login /> : <Navigate to="/" />} />
           <Route path="/registro_cliente" element={!token ? <RegistroCliente /> : <Navigate to="/" />} />
+        </Route>
+
+        {/* AQUÍ DEBEN IR LAS RUTAS DEL ADMINISTRADOR */}
+        <Route element={<ProtectedRoutes fallbackPath="/login" ranges={['Administrador']} />}>
+          <Route path="/admin/clientes" element={<ClientsPage />} />
         </Route>
       </Routes>
     </>
