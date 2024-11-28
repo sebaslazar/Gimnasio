c3891161406b561653868e23fcd5f152982a6eec
LUMAGOGO <l.rodriguez3@utp.edu.co>
2024-11-27 21:06:01 -0500

    Rewrite ProtectedRoute component as a Layout

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,29 +1,51 @@
-import { Route, Navigate } from "react-router";
-import { useUser } from "../contexts/UserContext";
+import { Navigate, Outlet } from 'react-router';
+import { useUser } from '../contexts/UserContext';
+import React from 'react';
+
+/** @type {{
+  ADMIN: 'Administrador',
+  USER: 'Cliente',
+  TRAINER: 'Entrenador',
+}} */
+export const AUTH_RANGES = {
+  ADMIN: 'Administrador',
+  USER: 'Cliente',
+  TRAINER: 'Entrenador',
+};
 
 /**
+ * @typedef {typeof AUTH_RANGES[keyof typeof AUTH_RANGES]} AuthRange
+ */
+
+/**
+ * @typedef {AuthRange | AuthRange[]} AuthRanges
+ */
+
+/**
+ * @description Should be used as a Layout component to protect routes
+ * 
  * @component
- * @extends {RouteProps}
  * @description A component that protects routes by checking authentication before rendering children
- * @param {import('react-router').RouteProps & {fallbackPath: string}} props - Component props
+ * @param {React.PropsWithChildren & {
+ *  fallbackPath: string,
+ *  ranges: AuthRanges,
+ * }} props - Component props
  * @returns {React.ReactElement|null} The protected route content if authenticated, null or redirect otherwise
  */
-export function ProtectedRoute({
+export function ProtectedRoutes({
   children,
-  element,
-  fallbackPath='/login',
-  ...rest
+  fallbackPath = '/login',
+  ranges,
+  ..._rest
 }) {
   const { token } = useUser();
 
-  return (
-    <Route
-      {...rest}
-      element={token ? element : 
-        <Navigate to={fallbackPath} replace />
-      }
-    >
-      {token ? children : null}
-    </Route>
+  return token ? (
+    <>
+      {children}
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to={fallbackPath} />
   );
 }
