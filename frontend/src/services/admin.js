4ca1d58777b3de859b49de688cafc83a8cb7aaf3
LUMAGOGO <l.rodriguez3@utp.edu.co>
2024-12-03 12:06:00 -0500

    update for empty lists management

--- frontend/src/services/admin.js
+++ frontend/src/services/admin.js
@@ -35,4 +35,111 @@ export function getClientes(token) {
       throw new Error('Error al cargar los datos');
     }
   })
+  .catch((error) => {
+    console.error(error);
+    if (error.response?.satus === 404) return [];
+    throw new Error('Error al cargar los datos');
+  });
+}
+
+/**
+ * 
+ * @param {string} token 
+ * @returns {Promise<Client[]>}
+ * @throws {Error}
+ */
+export function getAdministradores(token) {
+  return axios
+  .get('http://localhost:8888/admin/administradores', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+  .then(({ data, status }) => {
+    if (status === 200) {
+      return (data.resultado ?? []).map((client) => ({
+          nombre: client.nombre,
+          identificacion: client['ID_admin'],
+          telefono: client.telefono,
+          sexo: client.sexo,
+        }));
+    } else {
+      console.error(data);
+      throw new Error('Error al cargar los datos');
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    if (error.response?.satus === 404) return [];
+    throw new Error('Error al cargar los datos');
+  });
+}
+
+/**
+ * 
+ * @param {string} token 
+ * @returns {Promise<Client[]>}
+ * @throws {Error}
+ */
+export function getEntrenadores(token) {
+  return axios
+  .get('http://localhost:8888/admin/entrenadores', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+  .then(({ data, status }) => {
+    if (status === 200) {
+      return (data.resultado ?? []).map((client) => ({
+          nombre: client.nombre,
+          identificacion: client['ID_entrenador'],
+          telefono: client.telefono,
+          sexo: client.sexo,
+        }));
+    } else {
+      console.error(data);
+      throw new Error('Error al cargar los datos');
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    if (error.response?.satus === 404) return [];
+    throw new Error('Error al cargar los datos');
+  });
+}
+
+/**
+ * 
+ * @param {string} token 
+ * @returns {Promise<Client[]>}
+ * @throws {Error}
+ */
+export function getProveedores(token) {
+  return axios
+  .get('http://localhost:8888/admin/proveedores', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+  .then(({ data, status }) => {
+    if (status === 200) {
+      return (data.resultado ?? []).map((client) => ({
+          nombre: client.nombre,
+          identificacion: client['ID_proveedor'],
+          telefono: client.telefono,
+          direccion: client.direccion,
+        }));
+    } else {
+      console.error(data);
+      throw new Error('Error al cargar los datos');
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    if (error.response?.satus === 404) return [];
+    throw new Error('Error al cargar los datos');
+  });
 }
