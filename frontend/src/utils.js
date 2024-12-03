2f72b7865af5fe8d48a7479e4c24fc61731dfd0f
LUMAGOGO <l.rodriguez3@utp.edu.co>
2024-12-03 17:16:47 -0500

    Update regex in toPascalCase function to support Unicode letters

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,6 +4,6 @@
  * @returns 
  */
 export function toPascalCase(s){
-  return s.replace(/(\w)(\w*)/g,
+  return s.replace(/(\p{L})(\p{L}*)/gu,
     function(g0,g1,g2){return g1.toUpperCase() + g2.toLowerCase();});
 }
